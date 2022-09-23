abf85a091055afb78881cfa75c8b701c01dfd050
webdateng <webdateng@gmail.com>
2022-09-23 11:39:31 +0800

    add env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import userRoute from "./routes/UserRoute.js";
+require("dotenv").config();
 
 const app = express();
 mongoose.connect("mongodb+srv://angga:angga1234@mern.f27przc.mongodb.net/?retryWrites=true&w=majority", {
@@ -17,4 +18,4 @@ app.use(cors());
 app.use(express.json());
 app.use(userRoute);
 
-app.listen(5000, () => console.log("Server running..."));
+app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
