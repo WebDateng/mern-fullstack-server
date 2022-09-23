e784632092d1033d647fb5e39f4782177c08bd47
webdateng <webdateng@gmail.com>
2022-09-23 12:01:57 +0800

    set env

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import userRoute from "./routes/UserRoute.js";
-require("dotenv").config();
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 mongoose.connect("mongodb+srv://angga:angga1234@mern.f27przc.mongodb.net/?retryWrites=true&w=majority", {
